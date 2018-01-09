1d075014aa5a8bc6e074c892877ae398e55eb28a
Ben <bwhenry@syr.edu>
2018-01-08 19:02:11 -0700

    Initial creation of ElliGen solution and the command line project.

---
+++ ElliGen_CMD_Exec/ElliGen_CMD_Exec.cpp
@@ -0,0 +1,9 @@
+//---------------------------------------------------------
+// ElliGen_CMD_Exec provides a command line interface for 
+// running ElliGen.
+//---------------------------------------------------------
+
+int main()
+{
+	return 0;
+}
